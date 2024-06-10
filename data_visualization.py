96220fff042eedbb4e1ee92f806f02e958fb0d57
Emily <mocemmy@aol.com>
2024-06-10 10:29:04 -0400

    using python data visualization pandas functions

---
+++ data_visualization.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data = {
+    "Receipt ID": ["124DC", "4442A", "222BZ", None, "5421T"],
+    "Waiter/Waitress Name": ["Todd", None, "Lenny", "Jennifer", "Yazmin"],
+    "Tip Amount": [12, 4, 3, 44, 29]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+
+
+result = df.groupby("Team")["Score"].apply(lambda x: (x > 85).sum())
+
+print(result)
